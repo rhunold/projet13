912c75b5d0a337f25ccef6c9b016f5f1553c56c0
Hunold <raphael.hunold@gmail.com>
2023-05-24 15:41:48 +0200

    cleaning whitespace

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
 def trigger_error(request):
-    division_by_zero = 1 / 0
+    1 / 0
+
 
 urlpatterns = [
     path('', views.index, name='index'),
