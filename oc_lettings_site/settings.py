67e1c01a191313ac66c92e18ee0b599ccf61fd8a
Hunold <raphael.hunold@gmail.com>
2023-05-24 14:31:36 +0200

    modify settings to better manage sensible datas

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,9 +1,28 @@
 import os
 from dotenv import load_dotenv
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
-# Loand environments variables
+# Load environments variables
 load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_KEY'),
+    
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -15,11 +34,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 DEBUG = str(os.environ.get('DEBUG')) == "1"  # 1 == "True"
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 # if not DEBUG:
