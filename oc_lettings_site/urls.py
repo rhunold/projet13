df55111f69f0e1b327ccd0640407f20789785e0b
Hunold <raphael.hunold@gmail.com>
2023-05-24 15:25:05 +0200

    make pipeline public and add sentry example in url.py

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from . import views
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include("lettings.urls")),
     path('profiles/', include("profiles.urls")),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
