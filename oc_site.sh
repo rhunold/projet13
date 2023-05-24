77bcae44d2e7f9383c3b6d8b4c6a0d4a1370a624
Hunold <raphael.hunold@gmail.com>
2023-05-24 14:09:23 +0200

    delete files docker-compose.yml, oc_site.sh, .gitignore from distant repository

--- oc_site.sh
+++
@@ -1,9 +0,0 @@
-COMMIT_HASH=$(git rev-parse HEAD) \
-&& \
-docker build --build-arg COMMIT_HASH=$COMMIT_HASH -t rhunold/image_oc_letting:$COMMIT_HASH . \
-&& \
-docker push rhunold/image_oc_letting:$COMMIT_HASH \
-&& \
-docker pull rhunold/image_oc_letting:$COMMIT_HASH \
-&& \
-docker run --name container_oc_letting -p 8000:8000 rhunold/image_oc_letting:$COMMIT_HASH
